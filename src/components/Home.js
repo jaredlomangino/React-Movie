707622b9dcf930f2b0c338f3031328c349e2f898
jaredlomangino <jared.lomangino@gmail.com>
2020-07-08 14:47:16 -0400

    Added movie thumbnails in grid

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,8 +17,11 @@ import Spinner from "./elements/Spinner";
 
 import { useHomeFetch } from "./hooks/useHomeFetch";
 
+import NoImage from "./images/no_image.jpg";
+
 const Home = () => {
   const [{ state, loading, error }, fetchMovies] = useHomeFetch();
+  const [searchTerm, setSearchTerm] = useState("");
   console.log(state);
 
   if (!state.movies[0]) return <Spinner />;
@@ -31,7 +34,21 @@ const Home = () => {
         text={state.heroImage.overview}
       />
       <SearchBar />
-      <Grid />
+      <Grid header={searchTerm ? "Search Result" : "Popular Movies"}>
+        {state.movies.map((movie) => (
+          <MovieThumb
+            key={movie.id}
+            clickable
+            image={
+              movie.poster_path
+                ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`
+                : NoImage
+            }
+            movieId={movie.id}
+            movieName={movie.original_title}
+          />
+        ))}
+      </Grid>
       <MovieThumb />
       <Spinner />
       <LoadMoreBtn />
