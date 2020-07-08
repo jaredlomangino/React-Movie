194d732cdec6ca40cb56d5cf1a8c9f18de793bc9
jaredlomangino <jared.lomangino@gmail.com>
2020-07-08 19:42:20 -0400

    build package

---
+++ build/precache-manifest.ad9599278e244f66eb4463f4b95ca984.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1985a750658e54f04c5d827c84d13603",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5067ad9719dee8ef158",
+    "url": "/static/js/2.40ae298f.chunk.js"
+  },
+  {
+    "revision": "f4f2940a97ce468a69b6",
+    "url": "/static/js/main.84e23f15.chunk.js"
+  },
+  {
+    "revision": "7380a67bdde2a68b9cba",
+    "url": "/static/js/runtime~main.21d1648e.js"
+  },
+  {
+    "revision": "31f5bb1da5a478ef98a9213900c5f7a2",
+    "url": "/static/media/no_image.31f5bb1d.jpg"
+  },
+  {
+    "revision": "08494abf4210d1ea8b163791db70773c",
+    "url": "/static/media/reactMovie_logo.08494abf.png"
+  },
+  {
+    "revision": "30cd724bda43bdfa29216c8d8770b0c9",
+    "url": "/static/media/tmdb_logo.30cd724b.svg"
+  }
+]);
